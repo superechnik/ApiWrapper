ab8756a92cf5a96cff20d028b6a2ddc32f9a603b
Matthew Rosenberg <rosenberg.matthew@gmail.com>
2021-04-04 11:52:49 -0400

    add validation to mock api

--- Fake/Controllers/PropertyController.cs
+++ Fake/Controllers/PropertyController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using Lib.Models.PropertyDetails;
 using Lib.Models.HouseCanary;
 using Lib.Models.Sewer;
+using Microsoft.Extensions.Logging;
 
 namespace Fake
 {
@@ -13,10 +14,15 @@ namespace Fake
     public class PropertyController : ControllerBase
     {
         private readonly IResponseMocker _responseMocker;
+        private readonly ILogger<PropertyController> _logger;
 
-        public PropertyController(IResponseMocker responseMocker)
+        public PropertyController(
+            IResponseMocker responseMocker,
+            ILogger<PropertyController> logger
+            )
         {
             _responseMocker = responseMocker;
+            _logger = logger;
         }
 
         /// <summary>
@@ -24,14 +30,50 @@ namespace Fake
         /// </summary>
         /// <param name="address"></param>
         /// <param name="zipCode"></param>
+        /// <param name="unit"></param>
+        /// <param name="state"></param>
+        /// <param name="city"></param>
         /// <returns>
         /// HouseCanary.Root
         /// </returns>
         [HttpGet]
-        public async Task<IActionResult> Details([FromQuery] string address, int zipCode)
+        public async Task<IActionResult> Details(
+            [FromQuery] string address,
+            [FromQuery] string unit,
+            [FromQuery] string state,
+            [FromQuery] string city,
+            [FromQuery] string zipcode
+            )
         {
-            return await Task.Run( () => Ok(_responseMocker.MockResponse()));
-        
+
+            //validate url parameters
+            var lookups = new List<Lookup>()
+            {
+                new Lookup()
+                {
+                    Address = address,
+                    Unit = unit,
+                    State = state,
+                    City = city,
+                    Zipcode = zipcode
+                }
+            };
+
+            if (ValuesMissing(lookups))
+            {
+                return BadRequest("Url parameters must contain at least one non null value");
+            }
+            try
+            {
+                return await Task.Run(() => Ok(_responseMocker.MockResponse()));
+
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+
         }
 
         /// <summary>
@@ -44,15 +86,64 @@ namespace Fake
         [HttpPost]
         public async Task<IActionResult> Details([FromBody] IEnumerable<Lookup> lookup)
         {
-            IList<Root> listOfPropertyDetails = new List<Root>();
-
-            foreach (var propertyDetail in lookup)
+            //validate model
+            if (ValuesMissing(lookup))
             {
-                var response = await Task.Run(() => _responseMocker.MockResponse());
-                listOfPropertyDetails.Add(response);
+                return BadRequest("All request bodies must contain at least one non null value");
             }
 
-            return Ok(listOfPropertyDetails);
+            try
+            {
+                IList<Root> listOfPropertyDetails = new List<Root>();
+
+                foreach (var propertyDetail in lookup)
+                {
+                    var response = await Task.Run(() => _responseMocker.MockResponse());
+                    listOfPropertyDetails.Add(response);
+                }
+
+                return Ok(listOfPropertyDetails);
+
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+
+        }
+
+        /// <summary>
+        /// Iterates of list of request bodies and breaks if all values in any one of them are null
+        /// </summary>
+        /// <param name="lookups"></param>
+        /// <returns>
+        /// bool signifying if the values are missing
+        /// </returns>
+        private static bool ValuesMissing(IEnumerable<Lookup> lookups)
+        {
+            bool missingValue = false;
+
+            foreach (var lookup in lookups)
+            {
+                missingValue =
+                    lookup.Address is null ||
+                    (
+                        lookup.Zipcode is null &&
+                        (
+                            lookup.City is null ||
+                            lookup.State is null
+                        )
+
+                    );
+
+                if (missingValue)
+                {
+                    break;
+                }
+            }
+
+            return missingValue;
 
         }
 
--- Lib/models/propertyDetails/Lookup.cs
+++ Lib/models/propertyDetails/Lookup.cs
@@ -1,8 +1,16 @@
-﻿namespace Lib.Models.PropertyDetails
+﻿#nullable enable
+
+namespace Lib.Models.PropertyDetails
 {
+    /// <summary>
+    /// A model for a request body to the HouseCanary Property/Details api
+    /// </summary>
     public class Lookup
     {
-        public string Address { get; init; }
-        public int ZipCode { get; init; }
+        public string? Address { get; init; }
+        public string? Unit { get; init; }
+        public string? State { get; init; }
+        public string? City { get; init; }
+        public string? Zipcode { get; init; }
     }
 }
